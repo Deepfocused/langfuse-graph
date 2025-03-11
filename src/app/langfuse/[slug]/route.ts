b2c80b9422916e17a3f93f711703477e3fd5f4e9
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-11 17:11:46 +0900

    Update route.ts

--- src/app/langfuse/[slug]/route.ts
+++ src/app/langfuse/[slug]/route.ts
@@ -1,8 +1,10 @@
 // https://nextjs.org/docs/app/building-your-application/routing/route-handlers
+import { NextResponse } from 'next/server';
 
 export async function GET(
     request: Request,
-    { params }: { params: Promise<{ slug: string }> }
-  ) {
-    const { slug } = await params // 'a', 'b', or 'c'
-  }
+    { params }: { params: Promise<{ slug: string }> },
+) {
+    const { slug } = await params; // 'time', 'token', 'call', 'summary'
+    return NextResponse.json({ success: 'ok!' }, { status: 200 });
+}
