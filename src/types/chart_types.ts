10d8c6166cae3400a687d2f7da3aabf5af12c7e8
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-13 01:43:23 +0900

    Update chart_types.ts

--- src/types/chart_types.ts
+++ src/types/chart_types.ts
@@ -1,9 +1,9 @@
 import { ApexOptions } from 'apexcharts';
 
 // 사용 x
-export interface GraphProps {
-    height?: number;
-}
+// export interface GraphProps {
+//     height?: number;
+// }
 export interface ChartProps {
     series: ApexOptions['series'];
     options: ApexOptions;
