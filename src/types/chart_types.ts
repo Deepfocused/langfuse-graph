9f188c078d0c50375a9085160725015a6cca103f
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-13 03:05:52 +0900

    Update chart_types.ts

--- src/types/chart_types.ts
+++ src/types/chart_types.ts
@@ -8,3 +8,11 @@ export interface ChartProps {
     series: ApexOptions['series'];
     options: ApexOptions;
 }
+
+/**
+ 인덱스 시그니쳐(Index Signature)는 {[key : T] : U}형식으로 
+ 객체가 여러 Key를 가질 수 있으며 Key와 매칭되는 value를 가지는 경우 사용 
+ */
+export interface LlmCallCountData {
+    [key: string]: number;
+}
