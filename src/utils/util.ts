c5fa55f20d642a53e0a645cc04c78e5248fe9fa7
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-15 03:15:12 +0900

    Update util.ts

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,1 +1,21 @@
 
+import type { LlmType } from '@/types/chart_types';
+// 데이터 변환 함수
+export const transformDataForTime = (data: LlmType, x: string = 'Latency') => {
+    return Object.keys(data).map((key) => ({
+        name: key,
+        data: data[key].map((item: Array<number>) => ({
+            x: x,
+            y: [item[0], item[1]],
+        })),
+    }));
+};
+
+export const transformDataForSummary = (
+    data: Record<string, Array<number>>,
+) => {
+    return Object.keys(data).map((key) => ({
+        name: key,
+        data: data[key],
+    }));
+};
