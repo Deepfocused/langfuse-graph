44488e0eb03564874c77ecc37ec265e4e43c2ebd
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-18 19:41:45 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,56 @@
 'use client';
 
-import Link from 'next/link';
-import Time from '@/app/(graphs)/time/page';
-import Token from '@/app/(graphs)/token/page';
-import Call from '@/app/(graphs)/call/page';
-import Summary from '@/app/(graphs)/summary/page';
-import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import Contents from '@/components/content';
+import type { Infos } from '@/types/chart_types';
 
 export default function Home() {
     const height = 410;
     const titlefontSize = 20;
+
+    const [info, setInfo] = useState<Infos>({});
+    const [name, setName] = useState<string>('');
+    const [userId, setUserId] = useState<string>('');
+    const [traceId, setTraceId] = useState<string>('');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const url = new URL('/langfuse/info', window.location.origin);
+                const response = await fetch(url.toString());
+                if (response.ok) {
+                    let result;
+                    try {
+                        result = await response.json();
+                    } catch (jsonError) {}
+                    setInfo(result);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+        fetchData();
+        // 컴포넌트가 마운트될 때 인터벌 설정
+        const intervalId = setInterval(() => {
+            fetchData();
+        }, 600000); // 600초(10분)마다 fetchData 함수 호출
+
+        // 컴포넌트가 언마운트될 때 인터벌 정리
+        return () => clearInterval(intervalId);
+    }, []); // 빈 배열을 의존성 배열로 전달하여 한 번만 실행
+
+    // useEffect(() => {
+    //     console.log(info);
+    // }, [info]);
+
     return (
         <>
-            <div className="mt-2 mx-4 grid gap-6 md:grid-cols-2">
-                <motion.div
-                    initial={{ opacity: 0.0, scale: 0.7 }}
-                    animate={{
-                        scale: [1, 1, 1],
-                        opacity: [0.0, 0.0, 1],
-                        y: [3, 0],
-                    }}
-                    transition={{ duration: 0.2 }}
-                >
-                    <Link
-                        className="block border-2 rounded-xl border-fuchsia-200 shadow-lg shadow-fuchsia-300/50 transition hover:scale-103 hover:shadow-fuchsia-300"
-                        href="/time"
-                        scroll={false}
-                        prefetch={true}
-                    >
-                        <Time height={height} titlefontSize={titlefontSize} />
-                    </Link>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0.0, scale: 0.7 }}
-                    animate={{
-                        scale: [1, 1, 1],
-                        opacity: [0.0, 0.0, 1],
-                        y: [6, 0],
-                    }}
-                    transition={{ duration: 0.4 }}
-                >
-                    <Link
-                        className="block border-2 rounded-xl border-violet-200 shadow-lg shadow-violet-300/50 transition hover:scale-103 hover:shadow-violet-300"
-                        href="/token"
-                        scroll={false}
-                        prefetch={true}
-                    >
-                        <Token height={height} titlefontSize={titlefontSize} />
-                    </Link>
-                </motion.div>
-            </div>
-            <div className="mt-6 mx-4 grid gap-6 md:grid-cols-2">
-                <motion.div
-                    initial={{ opacity: 0.0, scale: 1 }}
-                    animate={{
-                        scale: [1, 1, 1],
-                        opacity: [0.0, 0.0, 1],
-                        y: [9, 0],
-                    }}
-                    transition={{ duration: 0.6 }}
-                >
-                    <Link
-                        className="block border-2 rounded-xl border-fuchsia-200 shadow-lg shadow-fuchsia-300/50 transition hover:scale-103 hover:shadow-fuchsia-300"
-                        href="/call"
-                        scroll={false}
-                        prefetch={true}
-                    >
-                        <Call height={height} titlefontSize={titlefontSize} />
-                    </Link>
-                </motion.div>
-                <motion.div
-                    initial={{ opacity: 0.0, scale: 0.7 }}
-                    animate={{
-                        scale: [1, 1, 1],
-                        opacity: [0.0, 0.0, 1],
-                        y: [12, 0],
-                    }}
-                    transition={{ duration: 0.8 }}
-                >
-                    <Link
-                        className="block border-2 rounded-xl border-violet-200 shadow-lg shadow-violet-300/50 transition hover:scale-103 hover:shadow-violet-300"
-                        href="/summary"
-                        scroll={false}
-                        prefetch={true}
-                    >
-                        <Summary
-                            height={height}
-                            titlefontSize={titlefontSize}
-                        />
-                    </Link>
-                </motion.div>
-            </div>
+            {/* name, userId, traceId select box*/}
+            <Contents
+                height={height}
+                titlefontSize={titlefontSize}
+                traceId={traceId}
+            />
         </>
     );
 }
