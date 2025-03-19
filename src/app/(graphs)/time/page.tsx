b376c20ed4bbe08381d9d322367e57443f53af84
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-19 15:43:25 +0900

    Update page.tsx

--- src/app/(graphs)/time/page.tsx
+++ src/app/(graphs)/time/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import type { ChartProps } from '@/types/chart_types';
-import { transformDataForTime as transformData } from '@/utils/util';
+import { transformDataForSummary as transformData } from '@/utils/util';
 import dynamic from 'next/dynamic';
 import { useInfo } from '@/context/InfoContext';
 
@@ -10,16 +10,9 @@ const ReactApexChart = dynamic(() => import('react-apexcharts'), {
     ssr: false,
 }); // browser에서만 렌더링해야하므로 ssr을 끔
 
-/*
-ContentsProps 대신 any를 써야하는 이유 
-Type error: Type 'OmitWithTag<ContentsProps, keyof PageProps, "default">' does not satisfy the constraint '{ [x: string]: never; }'.
-Property 'height' is incompatible with index signature.
-Type 'any' is not assignable to type 'never'.
-*/
-// 컴포넌트는 대문자
 const defaultChartOptions = (titlefontSize: number) => ({
     title: {
-        text: '🎢 Inference Timeline 🎢',
+        text: '🔊 Summary 🔊',
         align: 'center',
         style: {
             fontSize: `${titlefontSize}px`,
@@ -34,11 +27,11 @@ const defaultChartOptions = (titlefontSize: number) => ({
             offsetX: 25,
             offsetY: 0,
         },
-        type: 'rangeBar',
+        type: 'bar',
         foreColor: '#FFFFFF',
         dropShadow: {
             enabled: true,
-            color: '#FFFFFF',
+            color: '#fff',
             top: 0,
             left: 0,
             blur: 21,
@@ -48,12 +41,15 @@ const defaultChartOptions = (titlefontSize: number) => ({
             enabled: false,
         },
     },
-    // colors: ['#69d2e7', '#FF4560'],
     plotOptions: {
         bar: {
-            barHeight: '30%',
-            horizontal: true,
-            rangeBarGroupRows: true,
+            horizontal: false,
+            columnWidth: '60%',
+            borderRadius: 5,
+            borderRadiusApplication: 'end',
+            dataLabels: {
+                position: 'top',
+            },
         },
     },
     dataLabels: {
@@ -62,9 +58,16 @@ const defaultChartOptions = (titlefontSize: number) => ({
             fontSize: '14px',
             colors: ['#FFFFFF'],
         },
-        formatter: function (val: Array<number>): string {
-            return (val[1] - val[0]).toFixed(1) + 's';
+        offsetY: -18,
+        formatter: function (val: number) {
+            return val.toFixed();
         },
+    },
+    stroke: {
+        show: true,
+        width: 5,
+        curve: 'smooth',
+        colors: ['transparent'],
     },
     legend: {
         show: true,
@@ -78,68 +81,36 @@ const defaultChartOptions = (titlefontSize: number) => ({
         opacity: 1,
     },
     xaxis: {
-        stepSize: 5,
         labels: {
             show: true,
             style: {
                 fontSize: '14px',
             },
-            // formatter: (val: number): number => val,
         },
     },
     yaxis: {
         labels: {
-            show: false,
+            show: true,
             style: {
                 fontSize: '14px',
             },
+            formatter: (val: number): number => Math.round(val),
         },
     },
     grid: {
         xaxis: {
             lines: {
-                show: true,
+                show: false,
             },
         },
         yaxis: {
             lines: {
-                show: false,
+                show: true,
             },
         },
     },
     tooltip: {
         theme: 'dark',
-        custom: function (opts: any) {
-            const w = opts.ctx.w;
-            let ylabel = '';
-            if (
-                w.config.series[opts.seriesIndex].data &&
-                w.config.series[opts.seriesIndex].data[opts.dataPointIndex]
-            ) {
-                ylabel =
-                    w.config.series[opts.seriesIndex].data[opts.dataPointIndex]
-                        .x;
-            }
-            let seriesName = w.config.series[opts.seriesIndex].name
-                ? w.config.series[opts.seriesIndex].name
-                : '';
-            const color = w.globals.colors[opts.seriesIndex];
-
-            return (
-                '<div class="apexcharts-tooltip-rangebar">' +
-                '<div> <span class="series-name" style="color: ' +
-                color +
-                '">' +
-                (seriesName ? seriesName : '') +
-                '</span></div>' +
-                '<div> <span class="category">' +
-                ylabel +
-                ' </span> <span class="value start-value">' +
-                (opts.y2 - opts.y1) +
-                '</span></div>' +
-                '</div>'
-            );
-        },
     },
 });
 
@@ -150,8 +121,11 @@ Property 'height' is incompatible with index signature.
 Type 'any' is not assignable to type 'never'.
 */
 // 컴포넌트는 대문자
-
-export default function Time({ height = 640, titlefontSize = 28 }: any) {
+export default function Summary({
+    height = 640,
+    titlefontSize = 28,
+    showInfo = true,
+}: any) {
     const [state, setState] = useState<ChartProps>({
         series: [],
         options: defaultChartOptions(titlefontSize),
@@ -172,7 +146,10 @@ export default function Time({ height = 640, titlefontSize = 28 }: any) {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const url = new URL('/langfuse/time', window.location.origin);
+                const url = new URL(
+                    '/langfuse/summary',
+                    window.location.origin,
+                );
                 // if (name) url.searchParams.append('name', name);
                 // if (userId) url.searchParams.append('userId', userId);
                 // if (sessionId) url.searchParams.append('sessionId', sessionId);
@@ -198,6 +175,16 @@ export default function Time({ height = 640, titlefontSize = 28 }: any) {
                     setState((prevState) => ({
                         ...prevState,
                         series: transformedData,
+                        options: {
+                            xaxis: {
+                                categories: [
+                                    'latency(s)',
+                                    'Input Token',
+                                    'Output Token',
+                                    'Call Count',
+                                ],
+                            },
+                        },
                     }));
                 }
             } catch (error) {
@@ -213,50 +200,56 @@ export default function Time({ height = 640, titlefontSize = 28 }: any) {
 
     return (
         <>
-            <div className="flex justify-center gap-4">
-                <span className="rounded-md bg-gray-50 px-2 py-1 text-sm font-medium text-gray-600 ring-1 ring-gray-500/10">
-                    Project Name: {name}
-                </span>
-                <span className="rounded-md bg-red-50 px-2 py-1 text-sm font-medium text-red-700 ring-1 ring-red-600/10">
-                    User ID: {userId}
-                </span>
-                <span className="rounded-md bg-yellow-50 px-2 py-1 text-sm font-medium text-yellow-800 ring-1 ring-yellow-600/10">
-                    Session ID: {sessionId}
-                </span>
-                <span>
-                    <select
-                        value={traceId}
-                        onChange={(e) => {
-                            const selectedTraceId = e.target.value;
-                            setTraceId(selectedTraceId);
+            {showInfo && (
+                <div className="flex items-center justify-center gap-4">
+                    <span className="rounded-md bg-gray-100 px-1 py-1 text-sm font-medium text-gray-700 ring-4 ring-gray-700/50">
+                        Project Name : {name}
+                    </span>
+                    <span className="rounded-md bg-red-100 px-1 py-1 text-sm font-medium text-red-700 ring-4 ring-red-700/50">
+                        User ID : {userId}
+                    </span>
+                    <span className="rounded-md bg-yellow-100 px-1 py-1 text-sm font-medium text-yellow-700 ring-4 ring-yellow-700/50">
+                        Session ID : {sessionId}
+                    </span>
+                    <span>
+                        {/* sr-only : Screen Reader Only */}
+                        <label htmlFor="traceIdSelect" className="sr-only">
+                            Select Trace ID
+                        </label>
+                        <select
+                            id="traceIdSelect"
+                            value={traceId}
+                            onChange={(e) => {
+                                const selectedTraceId = e.target.value;
+                                setTraceId(selectedTraceId);
 
-                            if (selectedTraceId && info[selectedTraceId]) {
-                                const traceInfo = info[selectedTraceId];
-                                setName(traceInfo.name || '');
-                                setUserId(traceInfo.userId || '');
-                                setSessionId(traceInfo.sessionId || '');
-                            } else {
-                                setName('');
-                                setUserId('');
-                                setSessionId('');
-                            }
-                        }}
-                        className="py-1 px-2 block max-w-48 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-                    >
-                        <option value="">Select Trace ID</option>
-                        {Object.keys(info).map((id, index) => (
-                            <option key={index} value={id}>
-                                {id}
-                            </option>
-                        ))}
-                    </select>
-                </span>
-            </div>
+                                if (selectedTraceId && info[selectedTraceId]) {
+                                    const traceInfo = info[selectedTraceId];
+                                    setName(traceInfo.name || '');
+                                    setUserId(traceInfo.userId || '');
+                                    setSessionId(traceInfo.sessionId || '');
+                                } else {
+                                    setName('');
+                                    setUserId('');
+                                    setSessionId('');
+                                }
+                            }}
+                            className="px-1 py-1 max-w-48 rounded-lg border ring-1 ring-white-700/50"
+                        >
+                            {Object.keys(info).map((id, index) => (
+                                <option key={index} value={id}>
+                                    {id}
+                                </option>
+                            ))}
+                        </select>
+                    </span>
+                </div>
+            )}
             <ReactApexChart
                 className="mx-8 my-6"
                 options={state.options}
                 series={state.series}
-                type="rangeBar"
+                type="bar"
                 height={height}
             />
         </>
