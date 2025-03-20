809e3794f5db9c0657f6538eb2a318ff1a56f3a5
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-20 18:26:42 +0900

    Update page.tsx

--- src/app/(graphs)/time/page.tsx
+++ src/app/(graphs)/time/page.tsx
@@ -159,10 +159,10 @@ export default function Time({
     showInfo = true,
 }: any) {
     const fetchInterval: number = 600000;
-    const [state, setState] = useState<ChartProps>({
+    const [state, setState] = useState<ChartProps>(() => ({
         series: [],
         options: defaultChartOptions(titlefontSize),
-    });
+    }));
 
     const {
         name,
