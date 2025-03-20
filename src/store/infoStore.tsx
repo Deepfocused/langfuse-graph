c0bbee32e86ce7ae49a402a41a6748c0903a3f53
김종곤 <149566442+Deepfocused@users.noreply.github.com>
2025-03-20 22:13:54 +0900

    Update infoStore.tsx

--- src/store/infoStore.tsx
+++ src/store/infoStore.tsx
@@ -1,1 +1,17 @@
+'use client';
 
+import { create } from 'zustand';
+import type { InfoState } from '@/types/chartTypes';
+
+export const useInfoStore = create<InfoState>((set) => ({
+    name: '',
+    setName: (name) => set({ name }),
+    userId: '',
+    setUserId: (userId) => set({ userId }),
+    sessionId: '',
+    setSessionId: (sessionId) => set({ sessionId }),
+    traceId: '',
+    setTraceId: (traceId) => set({ traceId }),
+    info: {},
+    setInfo: (info) => set({ info }),
+}));
